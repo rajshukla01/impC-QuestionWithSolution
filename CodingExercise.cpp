61b28b080c8d6be7fe7c0007a515479700d33fd1
Raj Shukla <rajj444500@gmail.com>
2022-12-01 10:30:38 +0530

    Add files via upload

---
+++ CodingExercise.cpp
@@ -0,0 +1,15 @@
+//Topic:Program to write the area of a circle
+//Date:25/03/2022
+
+#include<iostream>
+using namespace std;
+const float pi=3.1416;
+int main(){
+    float fRadius,fAreaOfCircle;
+    cout<<"Enter the radius of the circle"<<endl;
+    cin>>fRadius;
+cout<<"The area of circle is :"<<pi*fRadius*fRadius<<endl;
+
+    
+    return 0;
+}
