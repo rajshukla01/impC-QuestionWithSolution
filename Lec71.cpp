8a6c0e11ff31c1da376bdcc4dae77c3f1107b422
Raj Shukla <rajj444500@gmail.com>
2022-12-01 10:44:16 +0530

    Add files via upload

---
+++ Lec71.cpp
@@ -0,0 +1,21 @@
+//Topic:Infinite loop demonstaration
+//Date:
+
+#include<iostream>
+using namespace std;
+int main(){
+    // while(1){
+    //     cout<<"my name is Raj Shukla"<<endl;
+    // }
+
+    
+    
+    
+    // Demonstration of for loop infinite loop
+
+    for(;;){ cout<<"Hello, World";
+
+    }
+    
+    return 0;
+}
