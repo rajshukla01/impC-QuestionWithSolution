9d72c389df178c8ee891a19df184d2a5d86e3a4d
Raj Shukla <rajj444500@gmail.com>
2022-12-01 10:25:12 +0530

    adding a file

---
+++ dfgfg.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"hello world";
+return  0;
+}
+
+//Topic:
+//Date:
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
---
+++ first_program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum;
+    num1=78;
+    num2=98;
+    sum=num1+num2;
+    cout<<"sum of num1+num2 ="<< sum;
+    return 0;
+}
