d9fd93aa2653aa550da498cd5be28477c4a97185
Raj Shukla <rajj444500@gmail.com>
2022-12-01 10:34:41 +0530

    Add files via upload

---
+++ Lec33.cpp
@@ -0,0 +1,15 @@
+//Topic:Operators 
+//Date:30/03/2022
+
+#include<iostream>
+using namespace std;
+int main(){
+    int ia,ib,ic,iSum;
+    ia=5;
+    ib=5;
+    ic=5;
+    iSum=0;
+    iSum+=ia+ib+ic;
+    cout<<iSum;
+    return 0;
+}
