6c2d8e5fcc88b5be7357260c78e51dd421ae1946
Raj Shukla <rajj444500@gmail.com>
2022-12-01 10:31:44 +0530

    Add files via upload

---
+++ Lec21.2.cpp
@@ -0,0 +1,15 @@
+//Topic:First program(Adding two number)
+//Date:12/03/2022
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the two number a,b for addition"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"The addition of "<<a<<" and "<< b <<" is "<<c;
+    return 0;    
+
+    
+}
