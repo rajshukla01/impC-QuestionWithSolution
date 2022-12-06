5850d25850b1d9cedfccc031ef126ff21fbf36a5
Raj Shukla <rajj444500@gmail.com>
2022-12-06 09:43:26 +0530

    vector array

---
+++ que1.cpp
@@ -0,0 +1,28 @@
+//Topic:
+//Date:
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(6);
+for(int i=0;i<v.size();i++){
+    cin>>v[i];
+}
+cout<<endl;
+for(int n=0;n<v.size();n++){
+    cout<<v[n];
+}
+//     for(int i=0;i<v.size();i++){
+//         cin>>v[i];
+//     }
+//     cout<<"The element taking is over"<<endl;
+//     for(int i=0;i<v.size();i++){
+//         cout<<v[i]<<" ";
+//     }
+//     cout<<"The size of the array is:"<<v.size()<<endl;
+//     v.pop_back();
+// // 
+    
+    return 0;
+}
