8f7b489b2ec4ac2550bd34abd1cecf97803e574a
Raj Shukla <rajj444500@gmail.com>
2022-12-08 08:53:25 +0530

    Add files via upload

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,32 @@
 //Topic:
 //Date:
 
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<4+-1;
-    
+    int n;
+    int j,k,count[100];
+    cin>>n; // no of wheels
+    int valueOfWheel[n],moves[n]; //current value on the wheel
+    for(int i=0;i<n;i++){
+        cin>>valueOfWheel[i];
+    }
+    for(j=0;j<n;j++){
+        cin>>moves[j];
+        char c[n][moves[j]];
+        for(k=0;k<moves[j];k++){
+            cin>>c[j][k];
+        }
+        for(int l=0;l<moves[j];l++){
+            if(c[j][l]=='D'){
+                count[l]++;
+            }
+        } 
+    }
+    for(int m=0;m<n;m++){
+        cout<<count[m]<<" "<<endl;
+    }
+   
+
     return 0;
 }
